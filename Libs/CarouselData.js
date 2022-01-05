bee371d8ee81d48e939abff1bc1094d441522f62
Adaji Mukhtar <alfadaji@gmail.com>
2022-01-05 08:22:58 +0100

    fixed image extension to work on vercel

--- Libs/CarouselData.js
+++ Libs/CarouselData.js
@@ -2,7 +2,7 @@ export const data = [
     {
       title: "Reliancecart",
       decription: "Landing for Reliancecart Grocery store, it fetches blog post from Wordpress REST API built with Nextjs and Bootstrap",
-      src: './img/reliancecart.png',
+      src: './img/reliancecart.PNG',
       alt: 'reliancecart website',
       url: 'https://reliancecart.ca/',
       target: '_blank'
@@ -10,7 +10,7 @@ export const data = [
     {
       title: "Reinkosta",
       decription: "Website for construction company, built with vanilla HTML, CSS, Javascript and Bootstrap with some cool css animation",
-      src: './img/reinkosta.png',
+      src: './img/reinkosta.PNG',
       alt: 'reinkosta website',
       url: 'http://reinkostaconstruction.com/',
       target: '_blank'
@@ -20,13 +20,13 @@ export const data = [
       decription: "Website for Niger State Signage and Advertisement Agency, Nigeria, built with nextjs, bootstrap and Strapi for backend of the blog",
       url: 'https://nissa-mu.vercel.app/',
       alt: 'nissaa',
-      src: './img/nissaa.png',
+      src: './img/nissaa.PNG',
       target: '_blank'
     },
     {
       title: "Bilkebab",
       decription: "A website for bilkebab Chops and coffee vendor, built with reactjs and bootstrap",
-      src: './img/bilkebab.png',
+      src: './img/bilkebab.PNG',
       alt: 'bilkebab',
       url: 'http://bilkebab.com/',
       target: '_blank'
@@ -34,7 +34,7 @@ export const data = [
     {
       title: "travel now",
       decription: "Converted a Figma design to code using vanilla html, css, javascript and Bootstrap",
-      src: './img/travelnow.png',
+      src: './img/travelnow.PNG',
       alt: 'travel now design',
       url: 'https://adaji025.github.io/travel-now/',
       target: '_blank'
@@ -42,7 +42,7 @@ export const data = [
     {
       title: "Bid win",
       decription: "Converted a Figma design to code using vanilla html, css, javascript and Bootstrap",
-      src: './img/bidwin.png',
+      src: './img/bidwin.PNG',
       alt: 'bid win design',
       url: ' https://adaji025.github.io/bidwin/',
       target: '_blank'
